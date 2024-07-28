b4638399e37b3df5f34d2a857692a10af7e2e3f7
Rishi Raj <98014028+rajRishi22@users.noreply.github.com>
2024-07-28 18:06:28 +0530

    Update index.js

--- Backend/index.js
+++ Backend/index.js
@@ -2,6 +2,8 @@ import express from "express";
 import mongoose from "mongoose";
 import dotenv from "dotenv";
 import cors from "cors";
+import path from "path";
+
 
 import bookRoute from "./route/book.route.js";
 import userRoute from "./route/user.route.js";
@@ -31,7 +33,17 @@ try {
 app.use("/book", bookRoute);
 app.use("/user", userRoute);
 
+
+
 //deployment
+if(process.env.NODE_ENV === 'production'){
+const dirPath=path.resolve();
+app.use(express.static(path.join(dirPath, '/Frontend/dist')));
+app.get("*",(req,res )=>
+    {
+        res.sendFile(path.resolve(dirPath, 'Frontend', 'dist', 'index.html'));
+    })
+}
 
 
 app.listen(PORT, () => {
