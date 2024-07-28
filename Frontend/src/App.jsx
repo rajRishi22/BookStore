ecc2c43fdbc6c8e0a6059cade4ac383d34c0a930
rajRishi22 <rishiraj14ap2004raj@gmail.com>
2024-07-28 15:49:19 +0530

    Added Contact and About Page

--- Frontend/src/App.jsx
+++ Frontend/src/App.jsx
@@ -5,7 +5,8 @@ import Courses from "./courses/Courses";
 import Signup from "./components/Signup";
 import { Toaster } from "react-hot-toast";
 import { useAuth } from "./context/AuthProvider";
-
+import About from "./components/About";
+import Contact from "./components/Contact";
 function App() {
   const [authUser, setAuthUser] = useAuth();
   console.log(authUser);
@@ -19,6 +20,9 @@ function App() {
             element={authUser ? <Courses /> : <Navigate to="/signup" />}
           />
           <Route path="/signup" element={<Signup />} />
+          
+          <Route path="/about" element={<About />} />
+          <Route path="/contact" element={<Contact/>} />
         </Routes>
         <Toaster />
       </div>
